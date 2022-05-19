c1a4c5c51ea882053bc2a40c17927a3897d7fb06
Aryan Marwaha <aryanmarwaha78@gmail.com>
2022-05-19 23:00:39 +0530

    original version 1

---
+++ C_plus plus.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class RECT
+{ 
+
+    public:
+    void area()
+    {
+        cout<<"Area of rectangle"<<endl;
+    }
+};
+
+class SQUARE
+{
+    public:
+    void area()
+    {
+        cout<<"Area of square";
+    }
+};
+
+class SHAPES : public RECT,public SQUARE    
+{
+};
+
+int main()    
+{ // This file is created by aryan
+    SHAPES obj;
+   //doing edit number 2
+    obj.RECT::area(); 
+    obj.SQUARE::area(); 
+}
